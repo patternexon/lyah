c6b475e08428498411d4b03d1677fe1b3bbb590e
patternexon <patternexon@gmail.com>
2012-01-06 23:29:42 -0500

    Chap2/Completed

--- baby.hs
+++ baby.hs
@@ -9,3 +9,21 @@ doubleSmallNumber x =	if x < 100
 		      	then doubleMe x
 		     	else x
 
+
+-- ' just means a none lazy version of the method or
+--  slightly different     
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+
+-- showing off the '
+-- and poor taste in standup
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+
+-- bangBoom
+boomBang xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+-- my own length function
+length' xs = sum [ 1 | _ <- xs]
+
+removeNonUpperCase xs = [ c | c <-xs, c `elem` ['A'..'Z']]
