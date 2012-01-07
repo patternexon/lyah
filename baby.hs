adaa3c91f2144cd7343c7391c9fc827f6cb004a8
patternexon <patternexon@gmail.com>
2012-01-06 19:23:04 -0500

    Chap 2/If statement

--- baby.hs
+++ baby.hs
@@ -2,3 +2,10 @@ doubleMe x = x + x
 
 --doubleUs x y = x*2 + y*2
 doubleUs x y = doubleMe x + doubleMe y
+
+
+-- first if statement
+doubleSmallNumber x =	if x < 100
+		      	then doubleMe x
+		     	else x
+
