b8a059ffc2bc4d5ba5e0ab293ac0e511814558c1
patternexon <patternexon@gmail.com>
2012-01-08 12:50:13 -0500

    Chap 3

---
+++ type.hs
@@ -0,0 +1,14 @@
+-- Chapter 3
+-- Types and typeclasses
+
+removeNonUpperCase :: [Char] -> [Char]
+removeNonUpperCase st = [ c| c <- st, c `elem` ['A'..'Z']]
+
+addThree ::Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [ 1..n ]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
