70b911dd873ae658b4b6a03ef895731b9d5e0ad0
patternexon <patternexon@gmail.com>
2012-01-21 10:38:30 -0500

    Initial Desc

---
+++ 99problems/naughties.hs
@@ -0,0 +1,5 @@
+-- Sat Jan 21 10:36:25 EST 2012
+-- Some exercises to practice stuff that I have learnt in the
+-- first 6 chapters of LYAH
+-- http://www.haskell.org/haskellwiki/99_questions/1_to_10
+
