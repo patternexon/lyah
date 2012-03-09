3783d4bd5e21a2b40cd6690992ec28a9995bb843
Akshat Shrivastava <patternexon@gmail.com>
2012-03-08 22:28:50 -0500

    palindromes and interact

---
+++ IO/palindromes.hs
@@ -0,0 +1,7 @@
+-- Chapter 8
+-- Palindromes and Interact
+
+main = interact respondPalindromes
+
+respondPalindromes = unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") . lines 
+    where isPalindrome xs = xs == reverse xs
