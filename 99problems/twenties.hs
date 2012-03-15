51c2eff0f57352c6b6d9687dba90d5fd5b244895
Akshat Shrivastava <patternexon@gmail.com>
2012-03-15 19:24:20 -0400

    Sol 21

---
+++ 99problems/twenties.hs
@@ -0,0 +1,9 @@
+-- Thu Mar 15 18:42:49 EDT 2012
+-- http://www.haskell.org/haskellwiki/99_questions/21_to_28
+-- Solution 21
+
+-- Solution to Problem 21
+insertAt :: (Eq a) => a -> [a] -> Int -> [a]
+insertAt x [] _ = [x]
+insertAt x ys n = left ++ [x] ++ right
+    where (left, right) = splitAt (n-1) ys
