350b6add3c143838d8e94503f3e8f53d897c6235
Akshat Shrivastava <patternexon@gmail.com>
2012-04-03 21:35:12 -0400

    Chapter 11/ Part 1

---
+++ Counter.hs
@@ -0,0 +1,7 @@
+-- Chapter 11
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter+1) (f x)
